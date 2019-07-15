1f09427d74f6ba300b80cc75b532b65251fd49e5
Andy Choi <wooseokchoi96@gmail.com>
2019-07-15 23:57:17 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,51 @@
 # My Code here....
+def map_to_negativize(array)
+  array.length.times{ |index|
+    array[index] = array[index] * (-1)
+  }
+  array
+end  
+
+def map_to_no_change(array)
+  array
+end
+
+def map_to_double(array)
+  array.length.times{ |index|
+    array[index] = array[index] * 2
+  }
+  array
+end
+
+def map_to_square(array)
+  array.length.times{ |index|
+    array[index] = array[index] ** 2
+  }
+  array
+end
+
+def reduce_to_total(array, starting_point = 0)
+  counter = starting_point
+  array.length.times{ |index|
+  counter+=array[index]
+  }
+  counter
+end
+
+def reduce_to_all_true(array)
+  array.all?
+end
+
+def reduce_to_any_true(array)
+  array.any?
+end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
